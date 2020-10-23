8feea0685248fd54a1bd7e402943c51d4c686473
damc-code <dalonso777@hotmail.com>
2020-10-22 22:05:09 -0600

    .

--- example_files/code/example.go
+++ example_files/code/example.go
@@ -6,12 +6,16 @@ import (
 	"time"
 )
 
+func prueba() {
+
+}
+
 // Guess the number: example taken from http://www.rosettacode.org/
 func main() {
 	fmt.Print("Guess number from k 1 to 10: ")
 	rand.Seed(time.Now().Unix())
 	n := rand.Intn(10) + 1
-
+	prueba()
 	for guess := n; ; fmt.Print("No. Try again: ") {
 		switch _, err := fmt.Scan(&guess); {
 		case err != nil:
